108685a5f7ac06aa6ba935797b64e9435f85ebf3
Adam Nagy <adam.nagy@autodesk.com>
2019-01-15 20:36:07 +0000

    First commit

---
+++ DeviceManager/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace DeviceManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.computerList = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.deviceList = new System.Windows.Forms.ListBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.copyProgress = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Available Books";
+            // 
+            // computerList
+            // 
+            this.computerList.FormattingEnabled = true;
+            this.computerList.Location = new System.Drawing.Point(15, 25);
+            this.computerList.Name = "computerList";
+            this.computerList.Size = new System.Drawing.Size(175, 173);
+            this.computerList.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Books on Device";
+            // 
+            // deviceList
+            // 
+            this.deviceList.FormattingEnabled = true;
+            this.deviceList.Location = new System.Drawing.Point(239, 25);
+            this.deviceList.Name = "deviceList";
+            this.deviceList.Size = new System.Drawing.Size(188, 173);
+            this.deviceList.TabIndex = 3;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(196, 52);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(37, 23);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = ">>";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(196, 131);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(37, 23);
+            this.removeButton.TabIndex = 5;
+            this.removeButton.Text = "<<";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // copyProgress
+            // 
+            this.copyProgress.Location = new System.Drawing.Point(15, 204);
+            this.copyProgress.Name = "copyProgress";
+            this.copyProgress.Size = new System.Drawing.Size(412, 11);
+            this.copyProgress.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 228);
+            this.Controls.Add(this.copyProgress);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.deviceList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.computerList);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Device Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox computerList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox deviceList;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.ProgressBar copyProgress;
+    }
+}
+
---
+++ DeviceManager/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.IO;
+using System.Collections.Specialized;
+
+namespace DeviceManager
+{
+    public partial class Form1 : Form
+    {
+        public class DeviceFolder
+        {
+            DirectoryInfo directoryInfo;
+
+            public DeviceFolder(DirectoryInfo info)
+            {
+                directoryInfo = info;
+            }
+
+            private string getSimplifiedName(string name)
+            {
+                var parts = name.Split(new char[] { '_' });
+                return parts[0];
+            }
+
+            public string getPath()
+            {
+                return directoryInfo.FullName;
+            }
+
+            public string getSimplifiedPath()
+            {
+                return Path.Combine(directoryInfo.Parent.FullName, getSimplifiedName(directoryInfo.Name));
+            }
+
+            public override string ToString()
+            {
+                return getSimplifiedName(directoryInfo.Name);
+            }
+        }
+
+        NameValueCollection appSettings = ConfigurationManager.AppSettings;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            fillList(computerList, appSettings["ComputerFolder"]);
+            fillList(deviceList, appSettings["DeviceFolder"]);
+        }
+
+        private void fillList(ListBox list, string folder)
+        {
+            DirectoryInfo info = new DirectoryInfo(folder);
+            DirectoryInfo[] directories = info.GetDirectories().OrderBy(p => p.Name).ToArray();
+            list.Items.Clear();
+            foreach (DirectoryInfo directory in directories)
+            {
+                if (!directory.Name.StartsWith("."))
+                    list.Items.Add(new DeviceFolder(directory)); 
+            }
+        }
+
+        private void copyFiles(string fromFolder, string toFolder)
+        {
+            DirectoryInfo info = new DirectoryInfo(fromFolder);
+            FileInfo[] files = info.GetFiles().OrderBy(p => p.Name).ToArray();
+            copyProgress.Maximum = files.Count<FileInfo>();
+            copyProgress.Value = 0;
+            Directory.CreateDirectory(toFolder);
+            foreach (FileInfo file in files)
+            {
+                File.Copy(file.FullName, Path.Combine(toFolder, file.Name), true);
+
+                copyProgress.Value++;
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (computerList.SelectedItem == null)
+            {
+                MessageBox.Show("Select an Available Book you want to copy to the Device");
+                return;
+            }
+
+            var selectedFolder = computerList.SelectedItem;
+            copyFiles(
+                (computerList.SelectedItem as DeviceFolder).getPath(),
+                Path.Combine(appSettings["DeviceFolder"], (computerList.SelectedItem as DeviceFolder).ToString())
+            );
+
+            fillList(deviceList, appSettings["DeviceFolder"]);
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (deviceList.SelectedItem == null)
+            {
+                MessageBox.Show("Select a Book you want to remove from the Device");
+                return;
+            }
+
+            Directory.Delete((deviceList.SelectedItem as DeviceFolder).getSimplifiedPath(), true);
+
+            deviceList.Items.Remove(deviceList.SelectedItem);
+        }
+    }
+}
